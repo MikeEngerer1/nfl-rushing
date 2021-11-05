5f83026c9ff3da6534129574ec759353a957e5d6
MikeEngerer1 <yoshi_832@hotmail.com>
2021-11-04 23:35:53 -0400

    add support for csv download; add limit, offset params for pagination

--- server.js
+++ server.js
@@ -1,68 +1,86 @@
+// import ENV variables
+require('dotenv').config();
+
+// import required dependencies
 const express = require('express'),
-      fs = require('fs'),
       bodyParser = require('body-parser'),
-      cors = require('cors'),
       moment = require('moment-timezone'),
       https = require('https'),
-      json2csv = require('json2csv'),
+      { Parser } = require('json2csv'),
       morgan = require('morgan');
 
-require('dotenv').config();
+// import db instance
+const { knex } = require('./db/db_instance');
+
+// import required models
+const Rushing = require('./models/rushing');
+
+// initialize the app
 const app = express();
 
-// middleware
-app.use(cors());
+// apply middleware
 app.use(bodyParser.json());
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(morgan('[:remote-addr] [:date[clf]] [:method :url]', {immediate: true}));
 app.use(express.static('public'));
 
-const Rushing = require('./models/rushing');
-const { knex } = require('./db/db_instance');
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-});
-
-app.get('/rushing/:name', (req, res) => {
-  const { name } = req.query;
-  if (name && typeof name === 'string') {
-    return res.render({})
-  }
-})
+// given a set of optional query params, either render the NFL Rushing Statistics page or send a CSV file 
 app.get('/rushing', (req, res) => {
-  const { csv, player } = req.query;
+  let { csv, Player, limit, offset } = req.query;
+  // parse string -> number; set default if not provided
+  limit = limit ? parseInt(limit) : 100;
+  offset = offset ? parseInt(offset) : 0;
+  // maximum 100 rows per request
+  if (limit > 100) {
+    // configure error info & render error page
+    const templateData = {status: 422, error: 'unprocessable entity', message: 'given limit is too large', data: {development: process.env.ENV === 'development'}}
+    return res.status(422).render('pages/error', {templateData});
+  }
   const opts = {}
-  if (player) opts.Player = player; 
-  return Rushing.list(opts).then(rushingData => {
+  if (Player) opts.Player = Player; 
+  // fetch all rushing data, count for given options
+  const rushingFetch = Rushing.list(opts, limit, offset),
+        rushingCountFetch = Rushing.listCount(opts); 
+  return Promise.all([rushingFetch, rushingCountFetch]).then(([rushingData, count]) => {
     if (!rushingData || !rushingData.length) {
-      const templateData = {status: 422, error: 'unprocessable entity', message: 'no data found for given query', data: null}
-      return res.render('pages/error', {templateData});
+      // configure error info & render error page
+      const templateData = {status: 422, error: 'unprocessable entity', message: 'no data found for given query', data: {development: process.env.ENV === 'development'}}
+      return res.status(422).render('pages/error', {templateData});
     }
-    rushingData = rushingData.map(player => {
-      delete player.id;
-      delete player.created_at;
-      delete player.updated_at;
-      return player
-    })
-    // if user requests a csv download, send the file rather than rendering the page
+    // if user requests a csv download, send the file
+    // otherwise render the page
     if (csv) {
-      // const /**/
+      try {
+        const { fields, rows } = Rushing.format('csv', rushingData);
+        const parser = new Parser({fields});
+        const csv = parser.parse(rows);
+        return res.attachment('rushing.csv').send(csv);
+      } catch (err) {
+        return Promise.reject(err);
+      }
     } else {
       const templateData = Rushing.format('html-table', rushingData);
+      templateData.meta = {
+        Player, // queried player
+        count, // total count
+        offset, // current offset
+        limit, // current limit
+        start: offset + 1, // displayed start of range
+        end: offset + limit < count ? offset + limit : count, // displayed end of range
+        previousPageOffset: offset - limit >= 0 ? offset - limit : null, // offset for previous page
+        nextPageOffset: offset + limit < count ? offset + limit : null // offset for next page 
+      }
       return res.render('pages/rushing', {templateData});
     }
   })
   .catch(err => {
-    console.log(err);
-    const templateData = {status: 500, error: err,  message: 'error while processing request data', data: null};
-    return res.render('pages/error', {templateData})
+    console.error('GET /rushing:', err);
+    // configure error info & render error page
+    const templateData = {status: 500, error: err,  message: 'error while processing request', data: {development: process.env.ENV === 'development'}}
+    return res.status(500).render('pages/error', {templateData});
   })
 })
 
-const server = app.listen(process.env.PORT, () => {
-  console.log(`HTTP server listening on port ${process.env.PORT}`)
-});
+// start the app on given PORT
+const server = app.listen(process.env.PORT, () => console.log(`HTTP server listening on port ${process.env.PORT}`));
